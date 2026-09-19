acb27dbf6f2afff5ec87edd9da8179de4f1b9be9
Vaibhav Ahuja <kochimonu@gmail.com>
2026-09-19 19:11:55 +0530

    Fix Settings page loading so contents render immediately

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,8 +1,6 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer"
 import Settings from './Settings';
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation"
 
 export const metadata = {
     title: 'My Settings',
@@ -10,15 +8,6 @@ export const metadata = {
 }
 
 export default async function SettingsPage() {
-    const supabase = createClient();
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
-
-    if (!user) {
-        redirect("/")
-    };
-
     return (
         <div className="min-h-screen bg-slate-50 text-slate-900">
             <Header />
