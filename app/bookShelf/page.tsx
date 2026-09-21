65e00d8f881acbb33d09d3c85aa5ed0fc8da3504
Vaibhav Ahuja <kochimonu@gmail.com>
2026-09-21 11:04:56 +0530

    Remove unused bookshelf import

--- app/bookShelf/page.tsx
+++ app/bookShelf/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link"
 import Header from "@/components/Header"
 import { Card, CardContent } from "@/components/ui/card"
 import { createClient } from "@/utils/supabase/server"
