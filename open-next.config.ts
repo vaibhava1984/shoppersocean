ee69247becebe7630e44d516e0220c93d0d36a14
Vaibhav Ahuja <kochimonu@gmail.com>
2026-09-26 12:42:35 +0530

    Add clean rebuild data and book flow

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,1 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig();
+import {defineCloudflareConfig} from "@opennextjs/cloudflare"; export default defineCloudflareConfig();
