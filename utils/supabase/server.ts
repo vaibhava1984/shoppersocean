010c6b2dcc0f1e520a47df06bfeb1fff4dc9ed30
Vaibhav Ahuja <kochimonu@gmail.com>
2026-09-26 11:55:01 +0530

    Remove remaining legacy Supabase files

--- utils/supabase/server.ts
+++
@@ -1,7 +0,0 @@
-import { createClient as createDbClient, getUser as getDbUser } from "@/utils/db/server";
-
-export function createClient() {
-  return createDbClient();
-}
-
-export const getUser = getDbUser;
